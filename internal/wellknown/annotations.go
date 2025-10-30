948cac42b65e6977be5276e68e3b38a822fb9c7b
Skyler Malinowski <skyler@schedmd.com>
2025-10-30 16:42:43 -0400

    refactor: use prefix const

--- internal/wellknown/annotations.go
+++ internal/wellknown/annotations.go
@@ -6,54 +6,54 @@ package wellknown
 const (
 	// AnnotationPlaceholderNode indicates the Node which corresponds to the
 	// the pod's placeholder job.
-	AnnotationPlaceholderNode = "slinky.slurm.net/slurm-node"
+	AnnotationPlaceholderNode = SlinkyPrefix + "slurm-node"
 
 	// AnnotationAccount overrides the default account
 	// for the Slurm placeholder job.
-	AnnotationAccount = "slinky.slurm.net/account"
+	AnnotationAccount = SlinkyPrefix + "account"
 	// AnnotationConstraint sets the constraint
 	// for the Slurm placeholder job.
-	AnnotationConstraints = "slinky.slurm.net/constraints"
+	AnnotationConstraints = SlinkyPrefix + "constraints"
 	// AnnotationCpuPerTask sets the number of cpus
 	// per task
-	AnnotationCpuPerTask = "slinky.slurm.net/cpu-per-task"
+	AnnotationCpuPerTask = SlinkyPrefix + "cpu-per-task"
 	// AnnotationGres overrides the default gres
 	// for the Slurm placeholder job.
-	AnnotationGres = "slinky.slurm.net/gres"
+	AnnotationGres = SlinkyPrefix + "gres"
 	// AnnotationGroupId overrides the default groupid
 	// for the Slurm placeholder job.
-	AnnotationGroupId = "slinky.slurm.net/group-id"
+	AnnotationGroupId = SlinkyPrefix + "group-id"
 	// AnnotationJobName sets the job name for
 	// the slurm job
-	AnnotationJobName = "slinky.slurm.net/job-name"
+	AnnotationJobName = SlinkyPrefix + "job-name"
 	// AnnotationLicenses sets the licenses
 	// for the Slurm placeholder job.
-	AnnotationLicenses = "slinky.slurm.net/licenses"
+	AnnotationLicenses = SlinkyPrefix + "licenses"
 	// AnnotationMaxNodes sets the maximum number of
 	// nodes for the placeholder job
-	AnnotationMaxNodes = "slinky.slurm.net/max-nodes"
+	AnnotationMaxNodes = SlinkyPrefix + "max-nodes"
 	// AnnotationMemPerNode sets the amount of memory
 	// per node
-	AnnotationMemPerNode = "slinky.slurm.net/mem-per-node"
+	AnnotationMemPerNode = SlinkyPrefix + "mem-per-node"
 	// AnnotationMinNodes sets the minimum number of
 	// nodes for the placeholder job
-	AnnotationMinNodes = "slinky.slurm.net/min-nodes"
+	AnnotationMinNodes = SlinkyPrefix + "min-nodes"
 	// AnnotationPartitions overrides the default partition
 	// for the Slurm placeholder job.
-	AnnotationPartition = "slinky.slurm.net/partition"
+	AnnotationPartition = SlinkyPrefix + "partition"
 	// AnnotationQOS overrides the default QOS
 	// for the Slurm placeholder job.
-	AnnotationQOS = "slinky.slurm.net/qos"
+	AnnotationQOS = SlinkyPrefix + "qos"
 	// AnnotationReservation sets the reservation
 	// for the Slurm placeholder job.
-	AnnotationReservation = "slinky.slurm.net/reservation"
+	AnnotationReservation = SlinkyPrefix + "reservation"
 	// AnnotationTimelimit sets the Time Limit in minutes
 	// for the Slurm placeholder job.
-	AnnotationTimeLimit = "slinky.slurm.net/timelimit"
+	AnnotationTimeLimit = SlinkyPrefix + "timelimit"
 	// AnnotationUserId overrides the default userid
 	// for the Slurm placeholder job.
-	AnnotationUserId = "slinky.slurm.net/user-id"
+	AnnotationUserId = SlinkyPrefix + "user-id"
 	// AnnotationWckey sets the Wckey
 	// for the Slurm placeholder job.
-	AnnotationWckey = "slinky.slurm.net/wckey"
+	AnnotationWckey = SlinkyPrefix + "wckey"
 )
---
+++ internal/wellknown/well_known.go
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: Copyright (C) SchedMD LLC.
+// SPDX-License-Identifier: Apache-2.0
+
+package wellknown
+
+// Prefixes
+const (
+	SlinkyPrefix = "slinky.slurm.net/"
+
+	SchedulerPrefix = "scheduler." + SlinkyPrefix
+)
